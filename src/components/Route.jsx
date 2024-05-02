5cc61835ac533ff22f507a1c88321643e9651072
Joseph Martínez <insertepala@gmail.com>
2024-05-01 22:04:50 -0500

    another test 3

--- src/components/Route.jsx
+++ src/components/Route.jsx
@@ -1,3 +1,5 @@
 export function Route ({ path, Component }) {
   return null
 }
+
+Route.displayName = 'Route'
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -12,7 +12,7 @@ export function Router ({ children, routes = [], defaultComponent: DefaultCompon
   console.log(currentPath)
   const childrenRoutes = Children.map(children, ({ props, type }) => {
     const { name } = type
-    console.log(name)
+
     if (name !== 'Route') return null
 
     return props
